6022c75df593d94d9ee1b1d968bcaf10cafd5671
Walker Frederick <walker@urdev.xyz>
2020-12-03 20:47:31 -0500

    qrcode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import QRCode from "react-qr-code";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Walker Was Here <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div style={{backgroundColor: "#041E41", height: '100vh', width: '100vw'}}>
+        <div style={{ width: '100%', display: 'flex', flexDirection: "row", justifyContent: 'center'}}>
+        <div style={{ marginTop: 30, backgroundColor: "#fff", padding: 30 ,display: 'flex', flexDirection: "row", justifyContent: 'center'}}>
+
+        <Switch>
+          <Route path="/">
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/:id" children={<QRCodeComp />} />
+        </Switch>
+        </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+function QRCodeComp() {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  let { id } = useParams();
+
+  return (
+    <div>
+      <QRCode value={id} />
     </div>
   );
 }
-
 export default App;
